86d602169a9e23c1d4f8ec2c602c12a0aa48998e
Superior-Snacks <joningiwork@gmail.com>
2025-02-06 16:27:59 +0000

    s

--- project.py
+++ project.py
@@ -6,8 +6,12 @@ if __name__ == "__main__":
 
 def pig_latin():
     # get input, translate
+    sentance = input(":")
+
+def translate_in():
+    #translate acording to rules
     ...
 
-def translate():
-    #translate acording to rules
+def translate_out():
+    #fix
     ...
