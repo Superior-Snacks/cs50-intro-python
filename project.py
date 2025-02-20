1a8e3dddac8ed38b561d58593f99864607600faf
Superior-Snacks <joningiwork@gmail.com>
2025-02-20 15:49:45 +0000

    s

--- project.py
+++ project.py
@@ -104,7 +104,7 @@ def check_data(data):
 
 #fix
 def reg_estimate(data):
-    pattern = re.match(r"[0-9]{1,2}[hour]|[hr]", data)
+    pattern = re.match(r"[0-9]{1,2}\.[0-9][hour]|[]", data)
     if pattern:
         return pattern
     else:
