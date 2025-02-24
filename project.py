e777508247a234ef336d1f5ff8d4b23525cfd362
Superior-Snacks <joningiwork@gmail.com>
2025-02-24 16:56:47 +0000

    s

--- project.py
+++ project.py
@@ -60,8 +60,8 @@ def sort_tasks():
 def view_tasks():
     table = texttable.Texttable()
     table.header(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
-    table.add_row(["Row 1, Col 1", "Row 1, Col 2", "Row 1, Col 3"])
-    table.add_row(["Row 2, Col 1", "Row 2, Col 2", "Row 2, Col 3"])
+    table.add_row(["Row 1, Col 1", "Row 1, Col 2", "Row 1, Col 3", "", "", "", ""])
+    table.add_row(["Row 2, Col 1", "Row 2, Col 2", "Row 2, Col 3", "", "", "", ""])
     print(table.draw())
 
 def planner_paramiter():
